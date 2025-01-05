17d9864830b3c7820ca338d58306fd48031e648b
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2025-01-05 16:51:07 +0300

    редактирование и чистка главной ветки

--- modules/botStates/InfoState.py
+++ modules/botStates/InfoState.py
@@ -11,16 +11,10 @@ class InfoState(BaseState):
     def init_internal(self):
         req = Request()
         user = req.get_user_info(self.user_name)
-        keyboard = InlineKeyboardMarkup()
-        copy_button = InlineKeyboardButton(
-            text="Скопировать ссылку",
-            callback_data=f"copy_link:{user.link}"
-        )
-        # keyboard.add(copy_button)
         self.send(f"Информация о вашем подключении:\n" +\
                   f"Скачано: {round(user.down / 1024**3, 1)} GB\n" +\
                   f"Загружено: {round(user.up / 1024**3, 1)} GB\n" +\
                   f"Последний день: {datetime.datetime.fromtimestamp(user.time / 1000).strftime("%d:%m:%Y")} дней\n" +\
-                  f"Ссылка для подключения:", reply_markup=keyboard)
+                  f"Ссылка для подключения:")
         self.send(f"```\n{user.link}\n```", parse_mode="MarkdownV2")
 
